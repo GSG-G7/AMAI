8d5fb05be8b4814625c78fe69bf2bd26ac227bac
Alaa <alaa.ktaima@gmail.com>
2019-07-03 15:26:34 +0300

    add read me file of our site

---
+++ JS/index.js
@@ -0,0 +1,35 @@
+const home = document.querySelector('section.home');
+const about = document.querySelector('section.about');
+const team = document.querySelector('section.team');
+const contact = document.querySelector('section.contact-us');
+const homeBtn = document.querySelector('nav a#home');
+const aboutBtn = document.querySelector('nav a#about');
+const teamBtn = document.querySelector('nav a#team');
+const contactBtn = document.querySelector('nav a#contact-us');
+const callBtn = document.querySelector('.home__btn__call');
+
+homeBtn.addEventListener('click', () => {
+    showSection(home);
+})
+aboutBtn.addEventListener('click', () => {
+    showSection(about);
+})
+teamBtn.addEventListener('click', () => {
+    showSection(team);
+})
+contactBtn.addEventListener('click', () => {
+    showSection(contact);
+})
+
+callBtn.addEventListener('click', () => {
+    showSection(contact);
+})
+
+function showSection(section) {
+    home.hidden = true;
+    about.hidden = true;
+    team.hidden = true;
+    contact.hidden = true;
+
+    section.hidden = false;
+}
