acd5c8186568bdd6fb3c9c646a2e2bb0c9c5b9b7
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-07-04 10:19:10 +0300

    styling effixts and edit conflects relates #100

--- JS/index.js
+++ JS/index.js
@@ -7,8 +7,11 @@ const aboutBtn = document.querySelector('nav a#about');
 const teamBtn = document.querySelector('nav a#team');
 const contactBtn = document.querySelector('nav a#contact-us');
 const callBtn = document.querySelector('.home__btn__call');
-
+const logo = document.querySelector(".logo");
 homeBtn.addEventListener('click', () => {
+    showSection(home);
+})
+logo.addEventListener('click', () => {
     showSection(home);
 })
 aboutBtn.addEventListener('click', () => {
@@ -30,6 +33,5 @@ function showSection(section) {
     about.hidden = true;
     team.hidden = true;
     contact.hidden = true;
-
     section.hidden = false;
 }
